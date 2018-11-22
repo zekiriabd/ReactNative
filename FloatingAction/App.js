6c4439036fe092ca45f395994de2f9d649356bed
Zekiriabd <Zekiriabd@yahoo.fr>
2018-11-23 00:39:43 +0100

    Add Menu Item

--- FloatingAction/App.js
+++ FloatingAction/App.js
@@ -1,33 +1,60 @@
 import React from 'react';
-import {TouchableOpacity,Image,StyleSheet, Text, View } from 'react-native';
+import {Animated,TouchableOpacity,Image,StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
  
  
+  constructor(){
+    super();
+    
+    this.TranslateYAnimation = new Animated.Value(50);
+    this.opacityAnimated = new Animated.Value(0);
+  }
+
+  floatBtnClicked = async () => {
+    this.TranslateYAnimation.setValue(50);  
+    this.opacityAnimated.setValue(0); 
+
+    Animated.timing( this.TranslateYAnimation, {toValue: 0,duration: 500 }).start();
+    Animated.timing(this.opacityAnimated, {toValue: 1, duration: 1000 }).start();
+  }
+
+
+  Item1Clicked = () => {alert('111111');}
+  Item2Clicked = () => {alert('222222');}
+  Item3Clicked = () => {alert('333333');} 
+
+
 
   
-  floatBtnClicked = async () => { 
-    alert('11111111');    
-   }
- 
- 
   render() {
-
-    const shadowStyle={
-      shadowOffset: { width: 1, height: 1 },
-      shadowColor: "#000000",
-      shadowOpacity: 0.5,
-      shadowRadius: 1,
-    }
-
-
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        
-      
-        <TouchableOpacity onPress={this.floatBtnClicked} style={[styles.floatView,shadowStyle]}>
-            <Image source={require('./assets/FloatingAction.png')} style={styles.floatimg} />
+       
+        <Animated.View style={
+          [
+            { transform:[{ translateY: this.TranslateYAnimation}]},
+            {opacity: this.opacityAnimated},
+            styles.iTemView
+          ]
+          }>  
+            <TouchableOpacity onPress={this.Item1Clicked} style={[styles.floatItemView]}>
+              <Text style={styles.ItemText}>Item 1</Text>
+              <Image source={require('./assets/image2.png')} style={styles.floatItemimg} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.Item2Clicked} style={[styles.floatItemView]}>
+            <Text style={styles.ItemText}>Item 2</Text>
+              <Image source={require('./assets/image3.png')} style={styles.floatItemimg} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.Item3Clicked} style={[styles.floatItemView]}>
+            <Text style={styles.ItemText}>Item 3</Text>
+              <Image source={require('./assets/image4.png')} style={styles.floatItemimg} />
+            </TouchableOpacity>
+        </Animated.View>
+
+        <TouchableOpacity onPress={this.floatBtnClicked} style={[styles.floatView]}>
+            <Image source={require('./assets/image1.png')} style={styles.floatimg} />
         </TouchableOpacity>
 
       </View>
@@ -35,10 +62,7 @@ export default class App extends React.Component {
   }
 }
 
-/*<TouchableOpacity style={[styles.floatBtn,shadowStyle]} onPress={this.floatBtnClicked}>
-<Text style={styles.textfloatBtn}>+</Text>        
-</TouchableOpacity>
-*/
+
 const styles = StyleSheet.create({
   
   container: {
@@ -59,25 +83,37 @@ const styles = StyleSheet.create({
     width:70,
     height:70,
   },
-
-
-  floatBtn:{
+  
+  iTemView:{
     position:'absolute',
-    backgroundColor:'red',
-    borderRadius:35,
-    width:70,
-    height:70,
-    bottom:35,
+    bottom:100,
     right:10,
-    alignItems:'center',
-    justifyContent:'center',
+    width:'50%',
   },
 
-  textfloatBtn:{
-    color:'#FFFFFF',
-    fontSize:40,
-    fontWeight:'bold',
-  }
+  floatItemView:{
+    marginRight:13,
+    flexDirection: 'row',
+    alignItems:'center' ,
+    alignSelf: 'flex-end',
+    height:60,
+  },
+
+ floatItemimg:{
+    width:50,
+    height:50,
+  },
+
+  ItemText:{
+    color:'#000000',
+    borderStyle: 'solid',
+    borderColor:'#666666',
+    borderRadius: 5,
+    borderWidth: 0.5,
+    padding:5,
+    marginRight:10,
+  },
+  
 
 
 
