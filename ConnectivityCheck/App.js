0cce9de292caacdf85eb6dedcefb3f3c005b71ea
zekiriabd <zekiriabd@gmail.com>
2019-01-25 21:18:46 +0100

    update

--- ConnectivityCheck/App.js
+++ ConnectivityCheck/App.js
@@ -1,62 +1,78 @@
-import React, { Component } from 'react';
-import {Text, Button,Label, View, StyleSheet, Alert } from 'react-native';
-import { Constants, BarCodeScanner, Permissions } from 'expo';
+import React from 'react';
+import { Text,Button,StyleSheet, NetInfo, View ,Animated } from 'react-native';
 
-export default class App extends Component {
-  state = {
-    hasCameraPermission: null,
-    showBarCodeScanner: false,
-    codeData:null,
-  };
+export default class App extends React.Component {
 
-  _requestCameraPermission = async () => {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({
-      hasCameraPermission: status === 'granted',
+  state ={
+    opLabel : new Animated.Value(1)
+  }
+
+  componentDidMount(){
+    NetInfo.addEventListener('connectionChange',this.OnConectivityChange);
+  }
+
+  componentWillUnmount(){
+    NetInfo.removeEventListener('connectionChange',this.OnConectivityChange);
+  }
+
+  OnConectivityChange = (conIfo) =>{
+    if(conIfo.type === 'none'){
+          Animated.timing(  this.state.opLabel, {toValue: 1, duration: 2000,}).start();
+    }
+    else{
+          Animated.timing(  this.state.opLabel, {toValue: 0, duration: 2000, }).start();   
+    }
+  }
+
+
+
+  btnChekConnection = () => {
+    NetInfo.isConnected.fetch().then(isConnected => {
+         alert(isConnected ? 'Is connected' : 'Is not connected');
     });
-  };
 
-  _handleBarCodeRead = data => {
-    this.setState({
-      codeData:data,
-      showBarCodeScanner:false,
+  }
+
+  btnNwetinfoClicked = () => {
+    NetInfo.getConnectionInfo().then(con => {
+         alert(
+           'Network connetion type : ' + con.type + ' ) '+
+           'EffectiveType :' + con.effectiveType + ' ) '         
+         );
     });
-  };
 
-btnCameraClicked = data  => {
-    this._requestCameraPermission();
+  }
 
-    this.setState({
-      codeData:null,
-      showBarCodeScanner:true,
-    });
-  };
 
 
   render() {
+    
     return (
       <View style={styles.container}>
-      
-        
-        <View>
-        {
-          this.state.showBarCodeScanner === true ?
-            <BarCodeScanner onBarCodeRead={this._handleBarCodeRead} style={{ height: 200, width: 200 }} /> :
-            <Button title="Take a picture" onPress={this.btnCameraClicked} />
-        }
-        <Text>{JSON.stringify(this.state.codeData)}</Text>
-        </View> 
+      <View style={styles.container}>
+          <Button title="Chek Connection" onPress={this.btnChekConnection}/>
+          <Button title="Get Network info" onPress={this.btnNwetinfoClicked}/>
+      </View>
+      <Animated.View style={[styles.opacityLabel,{opacity: this.state.opLabel}]}>
+         <Text>Is not connected</Text>
+      </Animated.View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-  }
+  },
+  
+  opacityLabel: {
+    padding:15,
+    justifyContent: 'flex-end',
+  },
+  
+ 
 });
